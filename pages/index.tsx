cfd219fa0b39aa095578b43c0b32d090326d9d20
unknown <shadab@8848digital.com>
2024-05-30 11:33:42 +0530

    feat:favicon added:sk

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import ExploreByCategory from "@/components/Explorecategory/ExploreByCategory";
 import FeaturedJobs from "@/components/Featuredjobs/FeaturedJobs";
 import { CONSTANTS } from "@/services/config/app-config";
 import { useEffect } from "react";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,12 @@ export default function Home() {
   if (CONSTANTS.CAREER_OPPORTUNITIES_PORTAL) {
     return (
       <>
+      <Head>
+        <title>8848 Digital-Job Portal</title>
+        <meta name="description" content="8848 Digital Job Portal - Find your dream job here" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon-1.png" />
+      </Head>
         <NavSection />
         <EasyStepSection />
         <ExploreByCategory />
